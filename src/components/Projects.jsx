64a66a85c76d5125889417061dd528f502e2fcb6
a <scalene04gushy@icloud.com>
2025-08-06 12:19:17 +0200

    Fix viewport issues

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ function Project() {
   return (
     <Section>
       <Header>Projects</Header>
-      <div className="grid grid-cols-3 gap-2 sm:flex-row">
+      <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 sm:gap-6 lg:grid-cols-3 ">
         <ProjectCard
           img="/pinselspur.png"
           title="Pinselspur"
